a37b29075bbe186895ceb93cb9236ed82d9ef0c2
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-19 04:30:41 +0200

    btn+classes

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-const btns = document.querySelectorAll('.tab-btn')
-const about = document.querySelector('.about')
-const articles = document.querySelectorAll('.content')
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll('.content');
 
-about.addEventListener('click', function (e) {
-  // console.log(e.target.dataset.id)
-  const id = e.target.dataset.id
+about.addEventListener('click', (e) => {
+  const id = e.target.dataset.id;
   if (id) {
-    // remove active from other buttons
-    btns.forEach(function (btn) {
-      btn.classList.remove('active')
-    })
-    e.target.classList.add('active')
-
-    // hide other articles
-    articles.forEach(function (article) {
-      article.classList.remove('active')
-    })
-    const element = document.getElementById(id)
-    element.classList.add('active')
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
+      e.target.classList.add('active');
+    });
   }
-})
+});
